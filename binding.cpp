0fae385441eadeb8b53ae7616b2349c9f705af46
kenji-tojo <kenjitojo@icloud.com>
2023-02-14 12:51:05 +0900

    add nanobind

---
+++ binding.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/tensor.h>
+
+#include "eigen-nn/module.hpp"
+
+
+namespace nb = nanobind;
+using namespace nb::literals;
+
+
+NB_MODULE(eignn, m) {
+
+    m.def("inspect_tensor", [](
+            nb::tensor<float, nb::shape<nb::any, nb::any>> &tensor
+    ) {
+        using namespace std;
+        cout << "tensor shape: " << tensor.shape(0) << "x" << tensor.shape(1) << endl;
+    });
+
+}
---
+++ test.py
@@ -0,0 +1,17 @@
+import numpy as np
+from PIL import Image
+import os, shutil
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', help='path to the input file')
+    args = parser.parse_args()
+
+    if args.file:
+        print(f'input file: {args.file}')
+    
+    import eignn as m
+    mat = np.zeros((10,20), dtype=np.float32)
+    m.inspect_tensor(mat)
