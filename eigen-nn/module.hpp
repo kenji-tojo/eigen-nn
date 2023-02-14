7618b5a106bfb68206cd13932d42c79277bcf9f5
kenji-tojo <kenjitojo@icloud.com>
2023-02-14 12:27:18 +0900

    add code

---
+++ eigen-nn/module.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+namespace enn {
+
+
+
+} // namespace enn
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "eigen-nn/module.hpp"
+
+int main() {
+    using namespace std;
+    using namespace Eigen;
+
+    auto v = Vector3f::Zero();
+    cout << v << endl;
+}
